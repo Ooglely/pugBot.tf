a873e2570893761e943032c64d2b8996f9523a09
Ooglely <alennavdic@gmail.com>
2025-01-08 20:51:18 -0600

    Change DB env name

--- lib/consts.ts
+++ lib/consts.ts
@@ -7,7 +7,7 @@ export const STEAM_API_KEY = process.env.STEAM_API_KEY || import.meta.env.STEAM_
 export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID || import.meta.env.DISCORD_CLIENT_ID;
 export const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET || import.meta.env.DISCORD_CLIENT_SECRET;
 
-export const DB_URL = process.env.DB_URL || import.meta.env.DB_URL;
+export const DB_URL = process.env.MONGO_URL || import.meta.env.MONGO_URL;
 export const PUGBOT_API_KEY = process.env.BOT_API_PASSWORD || import.meta.env.BOT_API_PASSWORD;
 export const PUGBOT_SITE_URL = `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` || "http://192.168.1.30:4321";
 export const GIT_SHA = process.env.RAILWAY_GIT_COMMIT_SHA || "unknown";
